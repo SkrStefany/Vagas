9578fae9f0e361f41b7fe6243609cdbe23c97ec8
Stefany Souza <sttefanyss14@gmail.com>
2025-02-13 21:44:47 -0300

    Portfolio Atualizado
    Atualização de foto
    Curriculo

--- Projeto/src/main/java/br/com/stefanymatsumoto/main/GestaoDeVagasApplication.java
+++ Projeto/src/main/java/br/com/stefanymatsumoto/main/GestaoDeVagasApplication.java
@@ -2,8 +2,10 @@ package br.com.stefanymatsumoto.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "br.com.stefanymatsumoto")
 public class GestaoDeVagasApplication {
 
 	public static void main(String[] args) {
---
+++ Projeto/src/main/java/br/com/stefanymatsumoto/main/modules/Candidatos/CandidatoEntity.java
@@ -0,0 +1,15 @@
+package br.com.stefanymatsumoto.main.modules.Candidatos;
+
+public class CandidatoEntity {
+
+    private UUID id;
+    private String name;
+    private String username;
+    private String email;
+    private String senha;
+    private String descrição;
+    private String curriculo;
+
+    
+    
+}
